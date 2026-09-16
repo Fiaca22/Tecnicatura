25712200bea3f91c8a6f23263fa37f7a0756b0d8
Alfonso Baridon Schlaps <alfonsobaridon@gmmail.com>
2026-09-16 16:42:18 -0300

    TP3 - Ej 1

---
+++ Programacion-2/TP3/1definiciones.py
@@ -0,0 +1,37 @@
+# Analice si las siguientes definiciones son correctas
+# a. Desde el punto de vista del diseño de un sistema orientado a objetos, una
+# clase es un “molde” que establece solamente los atributos de una entidad.
+
+# b. En la Programación Orientada a Objetos se busca tratar a toda entidad como
+# si fuera un objeto en la vida real, donde cada objeto tiene solamente
+# propiedades.
+
+# c. El diagrama de una clase sólo especifica los atributos del objeto.
+
+# d. Los objetos tienen atributos (características) y métodos (funciones)
+# asociados.
+
+# e. Un constructor es un método que se invoca cuando se crea un objeto.
+
+# f. Un comando es un método que nunca modifica los valores del objeto.
+
+# g. Una consulta es un método que modifica los valores del objeto.
+
+1
+
+# a. Incorrecta. Una clase no solo establece los atributos de una entidad, sino también los métodos que definen el
+# comportamiento de esa entidad.
+
+# b. Incorrecta. En la Programación Orientada a Objetos,
+# los objetos tienen tanto propiedades (atributos) como comportamientos (métodos), no solo propiedades.
+
+# c. Incorrecta. El diagrama de una clase especifica tanto los atributos como los métodos del objeto.
+
+# d. Correcta. Los objetos tienen atributos (características) y métodos (funciones) asociados.
+
+# e. Correcta. Un constructor es un método especial que se invoca cuando se crea un objeto, y se utiliza para
+# inicializar sus atributos.
+
+# f. Incorrecta. Un comando es un método que puede modificar los valores del objeto, no necesariamente nunca los modifica.
+
+# g. Incorrecta. Una consulta es un método que no modifica los valores del objeto, sino que simplemente los devuelve.
